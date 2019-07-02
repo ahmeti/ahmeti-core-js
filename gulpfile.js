d2b7819867032d94092906ca2f6001f69e976b21
Ahmet Imamoglu <ahmet363@gmail.com>
2019-07-02 15:42:39 +0300

    Fixed.

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,72 @@
 const gulp = require('gulp');
 const clean = require('gulp-clean');
+const rename = require('gulp-rename');
 
+gulp.task('MoveFolderAndFiles', function() {
+    return gulp.src([
+        'app/User.php'
+    ])
+        .pipe(gulp.dest('app/Modules/User/Models/'));
+});
 
 gulp.task('DeleteFolderAndFiles', function () {
     return gulp.src([
         'resources/js',
         'resources/sass',
         'resources/views/auth',
-        'rm resources/views/home.blade.php',
+        'resources/views/home.blade.php',
+        'resources/views/welcome.blade.php',
+        'public/css',
+        'public/js',
+        'resources/views/layouts/app.blade.php',
+        'app/User.php'
     ], {read: false})
         .pipe(clean());
 });
 
-
-gulp.task('CopyAuthAjaxRequest', function() {
-    return gulp.src('vendor/ahmeti/core/app/Http/Middleware/AuthAjaxRequest.php')
-        .pipe(gulp.dest('app/Http/Middleware/AuthAjaxRequest.php'));
+gulp.task('CopyAuthAjaxRequest', function () {
+    return gulp.src([
+        'vendor/ahmeti/core/app/Http/Middleware/AuthAjaxRequest.php'
+    ]).pipe(gulp.dest('app/Http/Middleware'));
 });
 
+gulp.task('CopyCoreCss', function () {
+    return gulp.src([
+        'node_modules/ahmeti-core-js/dist/css/**'
+    ]).pipe(gulp.dest('public/css'));
+});
+
+gulp.task('CopyCoreJs', function () {
+    return gulp.src([
+        'node_modules/ahmeti-core-js/dist/js/**'
+    ]).pipe(gulp.dest('public/js'));
+});
+
+gulp.task('CopyCoreFonts', function () {
+    return gulp.src([
+        'node_modules/ahmeti-core-js/dist/fonts/**'
+    ]).pipe(gulp.dest('public/fonts'));
+});
+
+gulp.task('CopyCoreImages', function () {
+    return gulp.src([
+        'node_modules/ahmeti-core-js/dist/images/**'
+    ]).pipe(gulp.dest('public/images'));
+});
+
+gulp.task('CopyWebpackSample', function () {
+    return gulp.src('node_modules/ahmeti-core-js/webpack.mix.sample')
+        .pipe(rename('webpack.mix.js'))
+        .pipe(gulp.dest('.'));
+});
 
 gulp.task('init', [
+    'MoveFolderAndFiles',
     'DeleteFolderAndFiles',
     'CopyAuthAjaxRequest',
-    'copy-data'
+    'CopyCoreCss',
+    'CopyCoreJs',
+    'CopyCoreFonts',
+    'CopyCoreImages',
+    'CopyWebpackSample'
 ]);
